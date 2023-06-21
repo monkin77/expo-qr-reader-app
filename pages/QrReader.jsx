6addd55bf503221016a8cb128c57887ca71a6306
Monkin <monkinsmurf@gmail.com>
2023-06-21 16:44:38 +0200

    Add initial QR Reader

--- pages/QrReader.jsx
+++ pages/QrReader.jsx
@@ -1,12 +1,45 @@
-import { useState } from "react";
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
-import Checkbox from "expo-checkbox";
-import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 const QrReader = ({ navigation }) => {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        };
+
+        getBarCodeScannerPermissions();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
         <View style={styles.container}>
-            <Text>QrReader</Text>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+
+            {scanned && (
+                <Button
+                    title={"Tap to Scan Again"}
+                    onPress={() => setScanned(false)}
+                />
+            )}
         </View>
     );
 };
